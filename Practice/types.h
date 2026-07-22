ad721edfccae78fbe56fb3d4e0d32116710f4f0f
KaveeshaOshan <kaveeshasamarakoon2005@gmail.com>
2026-07-22 22:20:47 +0530

    Making the Cell type enum

---
+++ Practice/types.h
@@ -0,0 +1,18 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef enum{
+
+    Square_Start,
+    Square_Property,
+    Square_Event,
+    Square_Tax,
+    Square_Railway,
+    Square_Special,
+    Square_Utility,
+    Square_Insurance,
+    Square_Bank
+
+} Square_Types;
+
+#endif
