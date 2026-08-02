65a23f518597ec366742d9cebed93394cb33b788
KaveeshaOshan <kaveeshasamarakoon2005@gmail.com>
2026-08-03 00:09:17 +0530

    Defining the players and initializing them

--- players.c
+++ players.c
@@ -1,21 +1,20 @@
 #include <string.h>
 #include "types.h"
 
-extern player players[4];
+extern Player players[4];
 
-void initializeSinglePlayer(player *p, short id, const char *name, StrategyType strategy)
+void initializeSinglePlayer(Player *p, short id, const char *name, StrategyType strategy)
 {
 
-    strcpy(p->name, name);
     p->strategy = strategy;
     p->ID = id;
 
     p->cash_balance = 30000;
     p->networth = 30000;
-    p->isBankrupt = 0;
+    p->isBankrupt = false;
 
     p->current_position = 0;          // GO
-    p->in_jail = 0;
+    p->in_jail = false;
     p->turns_remaining_Injail = 0;
 
     p->railwayOwned = 0;
@@ -24,20 +23,23 @@ void initializeSinglePlayer(player *p, short id, const char *name, StrategyType 
 
     for (int i = 0; i < 40; i++)
     {
-        p->ownedProperties[i] = -1;
+        p->ownedProperties[i] = false;
     }
 
-    p->loan.active = 0;
+    p->loan.loanAmount = 0;
+    p->loan.loanOutstanding = 0;
+    p->loan.startround = 0;
+    p->loan.status = LOAN_NONE;
     p->loan.principal = 0;
     p->loan.interestRate = 0;
-    p->loan.interestRate = 0;
     p->loan.roundsRemaining = 0;
+
 }
 
 void initializePlayers()
 {
-    initializeSinglePlayer(&players[0], 1, "Player 1", STRATEGY_AGGRESSIVE);
-    initializeSinglePlayer(&players[1], 2, "Player 2", STRATEGY_CONSERVATIVE);
-    initializeSinglePlayer(&players[2], 3, "Player 3", STRATEGY_RISK_TAKER);
-    initializeSinglePlayer(&players[3], 4, "Player 4", STRATEGY_OPPORTUNISTIC);
+    initializeSinglePlayer(&players[0], 0, "Aggressive Investor", STRATEGY_AGGRESSIVE);
+    initializeSinglePlayer(&players[1], 1, "Conservative Banker", STRATEGY_CONSERVATIVE);
+    initializeSinglePlayer(&players[2], 2, "Risk Taker", STRATEGY_RISK_TAKER);
+    initializeSinglePlayer(&players[3], 3, "Opportunistic Trader", STRATEGY_OPPORTUNISTIC);
 }
